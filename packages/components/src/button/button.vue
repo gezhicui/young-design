974f1ff46c098ce2a43fac996234c60df04ba1be
杨雨翔 <1372201820@qq.com>
2022-08-18 11:21:14 +0800

    feat:新增button组件

--- packages/components/src/button/button.vue
+++ packages/components/src/button/button.vue
@@ -1,9 +1,30 @@
 <template>
-  <button class="main">测试按钮321</button>
+  <button class="y-button" :class="styleClass">
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
 import './style/index.less';
-import { defineComponent } from 'vue';
-export default defineComponent({});
+import { defineComponent, computed } from 'vue';
+import { buttonProps } from './types';
+export default defineComponent({
+  name: 'y-button',
+  props: buttonProps,
+  setup(props) {
+    const styleClass = computed(() => {
+      return {
+        [`y-button--${props.type}`]: props.type,
+        'is-plain': props.plain,
+        'is-round': props.round,
+        'is-disabled': props.disabled,
+        [`y-button--${props.size}`]: props.size,
+      };
+    });
+
+    return {
+      styleClass,
+    };
+  },
+});
 </script>
--- packages/components/src/button/types.ts
+++ packages/components/src/button/types.ts
@@ -1,22 +1,26 @@
 
-
 import { ExtractPropTypes } from 'vue'
-export const ButtonType = ['default', 'primary', 'success', 'warning', 'danger']
-export const ButtonSize = ['large', 'normal', 'small', 'mini'];
+
+export const ButtonType = ['primary', 'success', 'info', 'warning', 'danger', 'text']
+export const ButtonSize = ['midium', 'small', 'mini'];
+
 export const buttonProps = {
   type: {
     type: String,
-    values: ButtonType
+    validator(value: string) {
+      //这里表示type只能接收这些值
+      return ButtonType.includes(value)
+    }
   },
   size: {
     type: String,
-    values: ButtonSize
-  }
+    validator(value: string) {
+      return ButtonSize.includes(value)
+    }
+  },
+  plain: Boolean,
+  round: Boolean,
+  disabled: Boolean
 }
+
 export type ButtonProps = ExtractPropTypes<typeof buttonProps>
-
-
-
-
-
-
