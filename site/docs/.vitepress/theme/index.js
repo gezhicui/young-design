5dcfb05030490765edd043f33c91d584b7920798
杨雨翔 <1372201820@qq.com>
2022-08-31 21:02:03 +0800

    build:组件es6导入和app.use兼容处理

--- site/docs/.vitepress/theme/index.js
+++ site/docs/.vitepress/theme/index.js
@@ -1,15 +1,13 @@
 // .vitepress/theme/index.js
 
 import DefaultTheme from 'vitepress/theme';
+import youngDesign from 'young-design';
 export default {
   ...DefaultTheme,
   enhanceApp: async ({ app, router, siteData, isServer }) => {
+    console.log(youngDesign);
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
-    import('young-design').then((module) => {
-      console.log(module);
-      app.use(module.Button);
-      app.use(module.Icon);
-    });
+    app.use(youngDesign);
   },
 };
