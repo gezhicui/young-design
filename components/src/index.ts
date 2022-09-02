e27b2926fc8772a4a09a8e4e52b7e87a29208578
杨雨翔 <1372201820@qq.com>
2022-09-02 14:22:45 +0800

    feat:新增link组件

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from './button'
-export { default as Icon } from './Icon'
+export { default as Icon } from './icon'
+export { default as Link } from './link'
--- examples/app.vue
+++ examples/app.vue
@@ -1,76 +1,29 @@
 <template>
   <div>
-    <Button>默认按钮</Button>
-    <Button type="primary">主要按钮</Button>
-    <Button type="success">成功按钮</Button>
-    <Button type="info">信息按钮</Button>
-    <Button type="warning">警告按钮</Button>
-    <Button type="danger">危险按钮</Button>
-    <Button type="text">文字按钮</Button>
+    <Link>link</Link>
     <br />
+    <Link type="primary">link</Link>
+    <Link type="primary" underline>link</Link>
+    <Link type="primary" underline disabled>link</Link>
     <br />
-    <Button plain>朴素按钮</Button>
-    <Button type="primary" plain>主要按钮</Button>
-    <Button type="success" plain>成功按钮</Button>
-    <Button type="info" plain>信息按钮</Button>
-    <Button type="warning" plain>警告按钮</Button>
-    <Button type="danger" plain>危险按钮</Button>
+    <Link type="info">link</Link>
+    <Link type="info" underline>link</Link>
+    <Link type="info" underline disabled>link</Link>
     <br />
+    <Link type="danger">link</Link>
+    <Link type="danger" underline>link</Link>
+    <Link type="danger" underline disabled>link</Link>
     <br />
-    <Button round>圆角按钮</Button>
-    <Button type="primary" round>主要按钮</Button>
-    <Button type="success" round>成功按钮</Button>
-    <Button type="info" round>信息按钮</Button>
-    <Button type="warning" round>警告按钮</Button>
-    <Button type="danger" round>危险按钮</Button>
+    <Link type="warning">link</Link>
+    <Link type="warning" underline>link</Link>
+    <Link type="warning" underline disabled>link</Link>
     <br />
-    <br />
-    <Button disabled>禁用按钮</Button>
-    <Button type="primary" disabled>主要按钮</Button>
-    <Button type="success" disabled>成功按钮</Button>
-    <Button type="info" disabled>信息按钮</Button>
-    <Button type="warning" disabled>警告按钮</Button>
-    <Button type="danger" disabled>危险按钮</Button>
-    <br />
-    <br />
-    <Button disabled>禁用按钮</Button>
-    <Button type="primary" disabled plain>主要按钮</Button>
-    <Button type="success" disabled plain>成功按钮</Button>
-    <Button type="info" disabled plain>信息按钮</Button>
-    <Button type="warning" disabled plain>警告按钮</Button>
-    <Button type="danger" disabled plain>危险按钮</Button>
-    <br />
-    <br />
-    <Button>默认按钮</Button>
-    <Button size="medium">中等按钮</Button>
-    <Button size="small">小型按钮</Button>
-    <Button size="mini">超小按钮</Button>
-    <div>
-      <Button type="success" icon="zuo">图标按钮</Button>
-      <Button type="primary" icon="zengjia" icon-position="right"
-        >图标按钮</Button
-      >
-      <Button type="primary" icon="zengjia"></Button>
-    </div>
-
-    <div class="icon-grpup">
-      <Icon name="zuo" />
-      <Icon name="you" color="red" dot />
-      <Icon name="yixuanze" dot badge="1" />
-    </div>
+    <Link type="success">link</Link>
+    <Link type="success" underline>link</Link>
+    <Link type="success" underline disabled>link</Link>
   </div>
 </template>
 <script lang="ts" setup>
-import { Button, Icon } from 'young-design';
+import { Link } from 'young-design';
 </script>
-
-<style lang="less">
-.y-button {
-  margin-right: 10px;
-}
-.icon-grpup .y-icon {
-  font-size: 36px;
-  color: #666;
-  margin-right: 20px;
-}
-</style>
+<style lang="less"></style>
