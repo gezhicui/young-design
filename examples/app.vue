b03e6211209e400f9a6ba5fc83652b61576cb692
杨雨翔 <1372201820@qq.com>
2022-10-09 21:39:37 +0800

    feat:新增select基本代码

--- examples/app.vue
+++ examples/app.vue
@@ -1,9 +1,35 @@
 <template>
-  <Switch />
+  <Select :options="options" disabled></Select>
 </template>
 
 <script lang="ts" setup>
-import Switch from "./src/Switch.vue";
+import Select from "./src/Select.vue";
+import { reactive } from "vue";
+const state = reactive({
+  options: [
+    {
+      label: "中国",
+      value: "china",
+    },
+    {
+      label: "日本",
+      value: "japan",
+    },
+    {
+      label: "美国",
+      value: "usa",
+    },
+    {
+      label: "俄罗斯",
+      value: "russia",
+    },
+    {
+      label: "韩国",
+      value: "korea",
+    },
+  ],
+});
+const { options } = state;
 </script>
 
 <style scoped>
---
+++ examples/src/Select.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="t-select" v-clickOutSide>
+    <input
+      type="text"
+      readonly
+      :value="selectVal"
+      placeholder="请选择"
+      :disabled="disabled"
+    />
+    <div class="t-position-box" v-if="positionShow">
+      <li v-for="item in options" :key="item.value" @click="change(item)">
+        {{ item.label }}
+      </li>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tSelect",
+};
+</script>
+
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  options: {
+    type: Array,
+    default: () => [],
+  },
+  disabled: Boolean,
+});
+console.log(props.options);
+const selectVal = ref("");
+const positionShow = ref(false);
+const vClickOutSide = {
+  beforeMount(el) {
+    const handler = (e) => {
+      if (!props.disabled) {
+        if (el.contains(e.target)) {
+          positionShow.value = true;
+        } else {
+          positionShow.value = false;
+        }
+      }
+    };
+    document.addEventListener("click", handler);
+  },
+};
+
+const change = (item) => {
+  selectVal.value = item.label;
+  positionShow.value = false;
+};
+</script>
+
+<style lang="less" scoped>
+@border: #d4d3d3;
+.t-select {
+  min-width: 250px;
+  display: inline-block;
+  height: 40px;
+  position: relative;
+  input {
+    border: 1px solid @border;
+    padding: 8px 10px;
+    outline: none;
+    width: 100%;
+    box-sizing: border-box;
+  }
+  .t-position-box {
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    position: absolute;
+    top: 42px;
+    border: 1px solid @border;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+    li {
+      list-style: none;
+      line-height: 40px;
+      padding: 0 10px;
+      box-sizing: border-box;
+      cursor: pointer;
+      font-size: 14px;
+      &:hover {
+        background: #f0f0f0;
+      }
+    }
+  }
+}
+</style>
