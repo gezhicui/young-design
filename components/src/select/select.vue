34f923dd5db7d12dcbd9ab49a3504a71be217e71
杨雨翔 <1372201820@qq.com>
2022-11-21 23:01:21 +0800

    feat:重构select组件,selectedLabel和selectvalue内部实现改造

--- components/src/select/select.vue
+++ components/src/select/select.vue
@@ -1,3 +1,5 @@
+<!-- selectedLabel  和selectvalue改造-->
+
 <template>
   <div :class="selectClass" :style="{ width: sizeChange(width) }" v-click-outside>
     <div :class="selectInputClass">
@@ -29,13 +31,15 @@
               @click="selChange(item, index)"
               :class="{
                 'y-select-active':
-                  activeIndex == index || selectedLabel == item[fieldLabel] || item.selected,
+                  activeIndex == index ||
+                  selectedValue == item[fieldValue] ||
+                  itemSelected(item[fieldValue]),
                 'y-select-disabled': item.disabled,
               }"
             >
               {{ item[fieldLabel] }}
               <Icon
-                v-if="multiple && item.selected"
+                v-if="multiple && itemSelected(item[fieldValue])"
                 name="check"
                 :icon-style="{ width: '13px', height: '13px' }"
               />
@@ -53,40 +57,30 @@ export default {
 };
 </script>
 <script lang="ts" setup>
-import { ref, computed, reactive } from 'vue';
+import { ref, computed, watchEffect, watch } from 'vue';
 import Icon from '../icon/icon.vue';
 import './style/index.less';
 import { sizeChange } from '../utils';
 import { selectProps } from './types';
 
 const emit = defineEmits(['update:modelValue', 'change']);
-
 const props = defineProps(selectProps);
-
-//初始化处理
-props.options.forEach((item, index) => {
-  if (!props.multiple) {
-    item.selected = false;
-  } else {
-    props.modelValue.forEach((item1, index1) => {
-      if (item[props.fieldValue] == item1) {
-        item.selected = true;
-      }
-    });
-  }
-});
 
 const activeIndex = ref(-1);
 const isShow = ref(false);
 const rotate = ref('rotate(0deg)');
 const optionsData: any = ref(props.options || []);
-const selectedLabel = ref(
-  props.multiple
-    ? props.modelValue
-    : props.modelValue != ''
-    ? props.options.find(item => item[props.fieldValue] == props.modelValue)[props.fieldLabel]
-    : ''
-);
+
+const itemSelected = value => {
+  if (Array.isArray(selectedValue.value)) {
+    return selectedValue.value.indexOf(value) !== -1;
+  } else {
+    return selectedValue.value === value;
+  }
+};
+
+const selectedLabel = ref();
+const selectedValue = ref(props.modelValue);
 
 // selece class
 const selectClass = computed(() => {
@@ -96,57 +90,49 @@ const selectClass = computed(() => {
 const selectInputClass = computed(() => {
   return ['y-select-input', { 'y-select-input-disabled': props.disabled }];
 });
-const blur = e => {
+const blur = () => {
   isShow.value = false;
   rotate.value = 'rotate(0deg)';
 };
 
-//输入查找
+// 输入查找
 const handleInput = e => {
   console.log('handleInput', e.target.value);
   selectedLabel.value = e.target.value;
   optionsData.value = [];
-  let filterList = props.options.filter(item => {
-    return item[props.fieldLabel].indexOf(e.target.value) != -1;
-  });
-  filterList.forEach((item, index) => {
-    optionsData.value.push(item);
+  optionsData.value = props.options.filter(item => {
+    return item[props.fieldLabel].includes(e.target.value);
   });
 };
+
+// 点击外部关闭下拉框
 const vClickOutside = {
   beforeMount(el) {
+    console.log('el', el);
+
     let handler = e => {
-      if (!props.disabled) {
-        if (!props.multiple) {
-          if (el.contains(e.target) && e.target.className.indexOf('y-select-option-li') == -1) {
-            if (!isShow.value) {
-              isShow.value = !isShow.value;
-              if (isShow.value) {
-                rotate.value = 'rotate(180deg)';
-              } else {
-                rotate.value = 'rotate(0deg)';
-              }
-            }
-          } else {
-            if (isShow.value) {
-              blur();
-            }
-          }
-        } else {
-          if (el.contains(e.target)) {
-            if (!isShow.value) {
-              isShow.value = !isShow.value;
-              if (isShow.value) {
-                rotate.value = 'rotate(180deg)';
-              } else {
-                rotate.value = 'rotate(0deg)';
-              }
-            }
-          } else {
-            if (isShow.value) {
-              blur();
-            }
-          }
+      if (props.disabled) return;
+
+      //点击的目标不在选项上
+      const notInPopup = e.target.className?.indexOf('y-select-option-li') == -1;
+
+      if (!props.multiple) {
+        // 单选处理
+        //点击input，且点击的不是下拉框的东西 且没打开下拉框
+        if (el.contains(e.target) && notInPopup && !isShow.value) {
+          isShow.value = !isShow.value;
+          rotate.value = `rotate(${isShow.value ? 180 : 0}deg)`;
+        } else if (isShow.value) {
+          //没点击input或没点击下拉框的东西
+          blur();
+        }
+      } else {
+        //多选处理
+        if (el.contains(e.target) && notInPopup) {
+          isShow.value = !isShow.value;
+          rotate.value = `rotate(${isShow.value ? 180 : 0}deg)`;
+        } else if (isShow.value && notInPopup) {
+          blur();
         }
       }
     };
@@ -161,35 +147,65 @@ const vClickOutside = {
     }
   },
 };
+
 // 选择事件
-let labels = [];
-let indexs = [];
 const selChange = (item, index) => {
   if (!props.multiple) {
     //单选
     if (!item.disabled) {
       activeIndex.value = index;
-      selectedLabel.value = item[props.fieldLabel];
-      emit('update:modelValue', item[props.fieldValue]);
-      emit('change', { lable: item.label, value: item.value, index: index });
+      selectedValue.value = item[props.fieldValue];
       blur();
     }
   } else {
     //多选
     if (!item.disabled) {
-      item.selected = !item.selected;
-      if (item.selected) {
-        selectedLabel.value.push(item[props.fieldValue]);
-        labels.push(item.label);
-        indexs.push(index);
+      if (itemSelected(item[props.fieldValue])) {
+        selectedValue.value = selectedValue.value.filter(v => v !== item[props.fieldValue]);
       } else {
-        selectedLabel.value = selectedLabel.value.filter(v => v !== item[props.fieldValue]);
-        labels = labels.filter(v => v !== item[props.fieldLabel]);
-        indexs = indexs.filter(v => v !== index);
+        selectedValue.value.push(item[props.fieldValue]);
       }
-      emit('update:modelValue', selectedLabel.value);
-      emit('change', { lable: labels, value: selectedLabel.value, index: indexs });
     }
   }
 };
+
+// watchEffect(() => {
+//   console.log('watchEffect');
+//   if (Array.isArray(selectedValue.value)) {
+//     //多选时选中的label数组
+//     selectedLabel.value = optionsData.value
+//       .filter(item => selectedValue.value?.includes(item[props.fieldValue]))
+//       .map(item => item[props.fieldLabel]);
+//   } else {
+//     //单选
+//     selectedLabel.value = optionsData.value.find(
+//       item => item[props.fieldValue] === selectedValue.value
+//     )?.[props.fieldLabel];
+//   }
+// });
+
+watch(
+  () => selectedValue.value,
+  (newvalue, oldvalue) => {
+    // console.log('watch', newvalue, oldvalue);
+
+    if (Array.isArray(selectedValue.value)) {
+      //多选时选中的label数组
+      selectedLabel.value = optionsData.value
+        .filter(item => selectedValue.value?.includes(item[props.fieldValue]))
+        .map(item => item[props.fieldLabel]);
+    } else {
+      //单选
+      selectedLabel.value = optionsData.value.find(
+        item => item[props.fieldValue] === selectedValue.value
+      )?.[props.fieldLabel];
+    }
+    emit('update:modelValue', selectedValue.value);
+    emit('change', {
+      lable: selectedLabel.value,
+      value: selectedValue.value,
+    });
+  },
+  { deep: true, immediate: true }
+);
 </script>
--- examples/src/Select.vue
+++ examples/src/Select.vue
@@ -10,18 +10,18 @@
 
 <script setup>
 import { reactive, ref } from 'vue';
-const selVal = ref([]);
+const selVal = ref(['1', '2']);
 const state = reactive({
   selOptions: [
-    { label: '北京', value: '131' },
-    { label: '天津', value: '332' },
-    { label: '上海', value: '289', disabled: true },
-    { label: '重庆', value: '132' },
-    { label: '深圳', value: '340' },
+    { label: '北京', value: '1' },
+    { label: '天津', value: '2' },
+    { label: '上海', value: '3', disabled: true },
+    { label: '重庆', value: '4' },
+    { label: '深圳', value: '5' },
   ],
 });
 const selchange = val => {
-  console.log(val);
+  console.log('selchange', val);
 };
 const { selOptions } = state;
 </script>
